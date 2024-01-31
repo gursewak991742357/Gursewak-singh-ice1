3e7dd12428bf57d454660d4b2ab5df670cfafd1b
Ebaad Zaidi <158239396+Ebaadz@users.noreply.github.com>
2024-01-31 08:06:31 -0500

    Update CardTrick.java
    lucky card added

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -8,7 +8,7 @@ package card;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ *@author ebaad
  */
 public class CardTrick {
     
@@ -16,17 +16,22 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
+        for (int i = 0; i < magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (Math.random() * 13) + 1); // Generate a random card value (1-13)
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]); // Generate a random suit (0-3)
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
+        // Add the luckyCard with card number 2 and suit "Clubs"
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
+        
+        // Insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        // Then report the result here
     }
     
 }
